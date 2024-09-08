38e9515def8cc6cdf7c5b4d62207982d9d6fbbcc
Alpesh Jamgade <alpeshjamgade21@gmail.com>
2024-09-08 13:40:05 +0530

    adds context based logger and stop on os interupption

--- app/app.go
+++ app/app.go
@@ -1,12 +1,19 @@
 package app
 
 import (
+	"context"
 	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"urlshortner/constants"
 	handler "urlshortner/handler/urls"
+	"urlshortner/logger"
 	repo "urlshortner/repo/urls"
 	service "urlshortner/service/urls"
+	"urlshortner/utils"
 )
 
 const (
@@ -14,17 +21,23 @@ const (
 )
 
 func Start() {
+
+	ctx := context.WithValue(context.Background(), constants.TRACE_ID, utils.GetUUID())
+	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	log := logger.CreateLoggerWithCtx(ctx)
+
 	urlsRepo := repo.NewUrlsRepo()
 	urlsService := service.NewUrlsService(urlsRepo)
 	urlsHandler := handler.NewUrlsHandler(urlsService)
 
 	r := mux.NewRouter()
 	urlsHandler.SetupRoutes(r)
-	err := http.ListenAndServe(fmt.Sprintf(":%s", http_port), r)
-	if err != nil {
-		println("Error starting server", "error", err)
-		return
-	}
-	println("Starting server", "port", http_port)
+	go func() {
+		http.ListenAndServe(fmt.Sprintf(":%s", http_port), r)
+	}()
 
+	<-ctx.Done()
+	log.Info("Shutting down server...")
 }
