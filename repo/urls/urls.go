3422ddb0e916d19128fa86f458421b52cb3b42ea
Alpesh Jamgade <alpeshjamgade21@gmail.com>
2024-09-14 08:53:00 +0530

    updated add url

--- repo/urls/urls.go
+++ repo/urls/urls.go
@@ -11,7 +11,7 @@ import (
 func (repo *UrlsRepo) GetAllUrls(ctx context.Context) ([]models.Url, error) {
 	log := logger.CreateLoggerWithCtx(ctx)
 	var urls []models.Url
-	sqlRow, err := repo.db.DB().Queryx("SELECT * FROM urls ORDER BY id DESC")
+	sqlRow, err := repo.db.DB().Queryx(`SELECT * FROM urls ORDER BY id DESC`)
 	if err != nil {
 		log.Errorw("error while fetching urls", "err", err)
 		return nil, err
@@ -20,7 +20,7 @@ func (repo *UrlsRepo) GetAllUrls(ctx context.Context) ([]models.Url, error) {
 		var url models.Url
 		err = sqlRow.StructScan(&url)
 		if err != nil {
-			log.Errorw("Error while scanning the row", "err", err)
+			log.Errorw("error while scanning the row", "err", err)
 			return nil, err
 		}
 		urls = append(urls, url)
